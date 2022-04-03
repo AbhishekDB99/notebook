7d18e877d77abb1a7be8d15b576c47f8e0cde587
AbhishekDB99 <abhishekborhade4@gmail.com>
2022-04-03 16:29:29 +0530

    latest changes

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose  = require("mongoose"); 
+require('dotenv').config({ path: '.env' });
+
 const connectToMongo = () => {
     mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-
 }
 module.exports= connectToMongo;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const connectToMongo = require('./db');
 var cors = require('cors')
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: ".env" });
 
 
 const express = require('express')
+require('dotenv').config({ path: '.env' });
+
 connectToMongo();
 const app = express()
 
@@ -12,15 +14,16 @@ app.use(cors())
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
-// ... other imports 
-const path = require("path")
 
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+  app.get("/",(req,res)=>{
+    const path1 = require("path");
+    res.sendFile(path1.resolve(__dirname,'client','build','index.html'));
+  })
+}
 
-// ...
-// Right before your app.listen(), add this:
-app.use('*', express.static(path.join(__dirname, "client", "build")))
+// console.log(path1.resolve(__dirname,'client','build','index.html'));
 
 
 app.use('/api/auth', require('./routes/auth'))
